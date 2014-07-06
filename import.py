15bef146f340124b32a976f0ee767910505597d5
David Konsumer <konsumer@jetboystudio.com>
2014-07-06 05:54:21 -0700

    add views in python

--- import.py
+++ import.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
-import couchdb
+from couchdb import Server, http
 import json
 import urllib
 
 config = json.loads(open('./config.json').read())
-couch = couchdb.Server("%s:%d" % (config['host'], config['port']))
+couch = Server("%s:%d" % (config['host'], config['port']))
 couch.resource.credentials = (config['auth']['username'], config['auth']['password'])
 
 try:
 	db = couch.create(config['database'])
-except couchdb.http.PreconditionFailed:
+except http.PreconditionFailed:
 	db = couch[config['database']]
 
+# create views
+db.save({
+  "_id": "_design/views",
+  "language": "javascript",
+  "views": {
+    "first": {
+      "map": "function (doc) { if (doc.type == 'first') emit(doc.count, doc.name); }"
+    },
+    "last": {
+      "map": "function (doc) { if (doc.type == 'last') emit(doc.count, doc.name); }"
+    },
+    "name": {
+      "map": "function (doc) { if (doc.type == 'full') emit(doc.name); }"
+    }
+  }
+})
+
+# import records
 types = {
 	'first': 'facebook-firstnames-withcount',
 	'last': 'facebook-lastnames-withcount',
 	'full': 'facebook-names-unique'
 }
-
 for t in types:
 	f = open('./data/%s.txt' % types[t], 'r')
 	for line in f:
