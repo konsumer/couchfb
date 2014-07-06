c6a70fe10db6adf89887ebe735c5ba22a69b995c
David Konsumer <konsumer@jetboystudio.com>
2014-07-06 02:26:50 -0700

    trying to dealwith concurrency

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var db = require('./db'),
   readline = require('readline'),
   path = require('path'),
   fs = require('fs'),
-  RateLimiter = require('limiter').RateLimiter,
-  limiter = new RateLimiter(20, 'second'); // no more than 20 requests per second
-
+  poolr  = require('poolr').createPool,
+  couchPool = poolr(2, db);
 
 var types = {
   'first':'facebook-firstnames-withcount',
@@ -12,28 +11,32 @@ var types = {
   'full':'facebook-names-unique'
 };
 
-// simple save callback
-function save(er, doc){
-  console.log(doc);
-}
-
 // process a text file, insert in couch
 function processFile(type){
   var fname = path.join('data', types[type] + '.txt');
   
   var rd = readline.createInterface({
     input: fs.createReadStream(fname),
+    output: process.stdout,
     terminal: false
   });
   
   rd.on('line', function(line) {
-    limiter.removeTokens(1, function(er, remainingRequests) {
-      if (type == 'full'){
-        db.save({type: 'full', name:line.trim().split(' ')}, save);
-      }else{
-        var i = line.trim().split(' ');
-        db.save({type: type, name: i[1].trim(), count: Number(i[0]) }, save);
-      }
+    var record = {
+      type: type
+    };
+    
+    if (type == 'full'){
+      record.name = line.trim().split(' ');
+    }else{
+      var i = line.trim().split(' ');
+      record.name = i[1].trim();
+      record.count = Number(i[0]);
+    }
+    
+    couchPool.addTask(db.save, record, function(er, res) {
+      if (er) return console.log('Error: ', er);
+      console.log(res);
     });
   });
 }
